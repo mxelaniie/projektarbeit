ce23c1e2cf08fad8b4fa1299066c33bf853f7cfc
mxelaniie <melanie.schiefermueller@students.fhnw.ch>
2025-12-17 11:36:02 +0100

    Vercel Orgin hinzugefügt für zugriff

--- Backend/main.py
+++ Backend/main.py
@@ -19,6 +19,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://localhost:5174", # falls zwei laufen
+    "https://projektarbeit-one.vercel.app/" #damit Vercel zugreifen kann
 ]
 
 app.add_middleware(
